b51c3f7f166f3044eb6b213a4941b94e284a8ebe
Oleksandr (Alik) Vovkotrub <alik@vovkotrub.com>
2025-04-05 16:22:12 +0300

    feat: improve

--- packages/example/src/app/frames.ts
+++ packages/example/src/app/frames.ts
@@ -1,5 +1,42 @@
+import { redirect } from "frames.js/core";
 import { createFrames } from "frames.js/next";
+import { isFrameDefinition } from "frames.js/utils";
 
 export const frames = createFrames({
   basePath: "/frames",
+  middleware: [
+    async (ctx, next) => {
+      const adDismissed = ctx.url.searchParams.get("ad_dismissed") === "true";
+      const adPath = ctx.url.pathname === "/frames";
+      const response = await next();
+      if (isFrameDefinition(response) && (adPath && !adDismissed)) {
+        const isViewAd = ctx.url.searchParams.get("view_ad") === "true";
+        if (isViewAd) {
+          return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ", response);
+        } else {
+          const adUrl = new URL(ctx.url);
+          adUrl.searchParams.set("view_ad", "true");
+          const adDismissedUrl = new URL(ctx.url);
+          adDismissedUrl.searchParams.set("ad_dismissed", "true");
+          response.image = "/../rps-ad.png";
+          response.imageOptions = {
+            aspectRatio: "1:1",
+          };
+          response.buttons = [
+            {
+              label: "Learn More",
+              action: "post_redirect",
+              target: adUrl.toString(),
+            },
+            {
+              label: "Dismiss",
+              action: "post",
+              target: adDismissedUrl.toString(),
+            },
+          ];
+        }
+      }
+      return response;
+    },
+  ]
 });
--- packages/example/src/app/frames/route.tsx
+++ packages/example/src/app/frames/route.tsx
@@ -4,37 +4,10 @@ import { frames } from "@/app/frames";
 const handleRequest = frames(async (_ctx) => {
   return {
     target: "/result",
-    image: (
-      <div
-        style={{
-          alignItems: "center",
-          background: "black",
-          backgroundSize: "100% 100%",
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "nowrap",
-          height: "100%",
-          justifyContent: "center",
-          textAlign: "center",
-          width: "100%",
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-            fontSize: 60,
-            fontStyle: "normal",
-            letterSpacing: "-0.025em",
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: "0 120px",
-            whiteSpace: "pre-wrap",
-          }}
-        >
-          Choose your weapon
-        </div>
-      </div>
-    ),
+    image: "/../rps.png",
+    imageOptions: {
+      aspectRatio: "1:1",
+    },
     buttons: [
       <Button key="rock" action="post" target="/result">
         Rock
